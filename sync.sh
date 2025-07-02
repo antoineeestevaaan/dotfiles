5292ac620746dc3ec41b9027ae8967588a20a315
antoine <stevan.antoine@gmail.com>
2025-07-02 19:32:20 +0200

    add script to sync files with $HOME

---
+++ sync.sh
@@ -0,0 +1,14 @@
+RED="\e[31m"
+MAGENTA="\e[35m"
+LIGHT_MAGENTA="\e[95m"
+RESET="\e[0m"
+
+for f in $(find . -type f -not -path './.git/*' -not -path './sync.sh'); do
+    f="$(echo $f | cut -c 3-)"
+
+    src="$(realpath $f)"
+    dest="$HOME/$f"
+
+    echo -e $"($RED$f$RESET): $MAGENTA$src$RESET -> $LIGHT_MAGENTA$dest$RESET"
+    ln --symbolic --force $src $dest
+done
