c63ef068fb869a6e77f58dee7eb62610246c7039
antoineeestevaaan <antoine.stevaaan@gmail.com>
2025-07-03 22:55:15 +0200

    skip scripts on sync

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@ MAGENTA="\e[35m"
 LIGHT_MAGENTA="\e[95m"
 RESET="\e[0m"
 
-for f in $(find . -type f -not -path './.git/*' -not -path './sync.sh' -not -path './gh.sh' -not -path './nu.sh'); do
+for f in $(find . -type f -not -path './.git/*' -not -path './sync.sh' -not -path './gh.sh' -not -path './nu.sh' -not -path './scripts/*'); do
     f="$(echo $f | cut -c 3-)"
 
     src="$(realpath $f)"
