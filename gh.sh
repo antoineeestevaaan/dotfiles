d40972d9b2d328f913684ec5032337b88b985e83
antoineeestevaaan <antoine.stevaaan@gmail.com>
2025-07-08 17:19:30 +0200

    install gh man pages locally

--- gh.sh
+++ gh.sh
@@ -1,6 +1,6 @@
 BIN="$HOME/opt/bin"
 TMP="/tmp"
-MAN="/usr/local/share/man/man1"
+MAN="$HOME/.local/share/man/man1"
 DOWNLOADS="$HOME/downloads"
 
 VERSION="2.74.2"
@@ -13,11 +13,11 @@ URL="https://github.com/cli/cli"
 mkdir -p "$BIN"
 mkdir -p "$DOWNLOADS"
 mkdir -p "$TMP"
-sudo mkdir -p "$MAN"
+mkdir -p "$MAN"
 
 curl -fLo "$DOWNLOADS/$ARCHIVE" "$URL/releases/download/v$VERSION/$ARCHIVE"
 tar xvf "$DOWNLOADS/$ARCHIVE" --directory "$TMP"
 cp "$TMP/$ARCHIVE_WITHOUT_EXT/bin/gh" "$BIN/gh"
 for f in $(find "$TMP/$ARCHIVE_WITHOUT_EXT/share/man/man1" -type f); do
-    sudo cp --verbose "$f" "$MAN"
+    cp --verbose "$f" "$MAN"
 done
