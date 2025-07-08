962c81725861c8c53cbf0cb3f35ec33e1f8c4e09
antoineeestevaaan <antoine.stevaaan@gmail.com>
2025-07-08 17:20:17 +0200

    pass gh architecture as argument

--- gh.sh
+++ gh.sh
@@ -6,19 +6,24 @@ MAN="$HOME/.local/share/man/man1"
 DOWNLOADS="$HOME/downloads"
 
 VERSION="2.74.2"
-ARCH="linux_armv6"
+ARCH="linux_$1"
 
 ARCHIVE_WITHOUT_EXT="gh_${VERSION}_${ARCH}"
 ARCHIVE="${ARCHIVE_WITHOUT_EXT}.tar.gz"
 URL="https://github.com/cli/cli"
+
+zipped="$DOWNLOADS/$ARCHIVE"
+remote="$URL/releases/download/v$VERSION/$ARCHIVE"
 
 mkdir -p "$BIN"
 mkdir -p "$DOWNLOADS"
 mkdir -p "$TMP"
 mkdir -p "$MAN"
 
-curl -fLo "$DOWNLOADS/$ARCHIVE" "$URL/releases/download/v$VERSION/$ARCHIVE"
-tar xvf "$DOWNLOADS/$ARCHIVE" --directory "$TMP"
+echo "fetching $remote"
+curl -#fLo "$zipped" "$remote" || exit 1
+
+tar xvf "$zipped" --directory "$TMP"
 cp "$TMP/$ARCHIVE_WITHOUT_EXT/bin/gh" "$BIN/gh"
 for f in $(find "$TMP/$ARCHIVE_WITHOUT_EXT/share/man/man1" -type f); do
     cp --verbose "$f" "$MAN"
