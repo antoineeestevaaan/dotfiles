d624e1af876ceb048d86a2923d544495247d03b4
antoineeestevaaan <antoine.stevaaan@gmail.com>
2025-07-12 18:23:30 +0200

    nvim: pass range to Nushell "formatter" for global and visual

--- .config/nvim/after/ftplugin/nu.lua
+++ .config/nvim/after/ftplugin/nu.lua
@@ -1,12 +1,12 @@
-local format = function()
+local format = function(range)
   if vim.bo.filetype ~= "nu" then
     print "not a Nushell buffer"
   else
-    print "formatting"
-    pcall(function() vim.cmd([[:%s/\([a-zA-Z"\])}0-9]\)\s*|\s*/\1\r    | /g]]) end)
-
-    pcall(function() vim.cmd([[%s/^\s*|/    |/]]) end)
+    print("formatting " .. range)
+    pcall(function() vim.cmd(":" .. range .. [[s/\([a-zA-Z"\])}0-9]\)\s*|\s*/\1\r    | /g]]) end)
+    pcall(function() vim.cmd(":" .. range .. [[s/^\s*|/    |/]]) end)
   end
 end
 
-vim.keymap.set("n", "<leader>lf", format)
+vim.keymap.set("n", "<leader>lf", function() format("%") end)
+vim.keymap.set("v", "<leader>lf", function() format("'<,'>") end)
