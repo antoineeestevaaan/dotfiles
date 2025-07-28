1e3cd0c6b2a07044e6cb2f0312d9573ab81c0b2b
antoineeestevaaan <antoineee.stevaaan@gmail.com>
2025-07-28 20:16:36 +0200

    bootstrap: add support for release/cargo install methods

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DEFAULT_BIN_DIR="$HOME/opt/bin"
+DEFAULT_INSTALL_METHOD="release"
 
 usage() {
     echo "Usage: $0 <Nushell version> [--bin <path> = $DEFAULT_BIN_DIR]"
@@ -13,6 +14,7 @@ if [[ $# -lt 1 ]]; then
 fi
 
 bin_dir="$DEFAULT_BIN_DIR"
+install_method="$DEFAULT_INSTALL_METHOD"
 version="$1"
 shift
 
@@ -26,6 +28,10 @@ while [[ $# -gt 0 ]]; do
             fi
             bin_dir=$(realpath "$1")
         ;;
+        --cargo)
+            shift
+            install_method="cargo"
+        ;;
         *)
             echo "Error: unknown argument $1"
             usage
@@ -35,15 +41,30 @@ while [[ $# -gt 0 ]]; do
 done
 # argument parsing: end
 
-arch=$(uname -m)
+case "$install_method" in
+    "release")
+        arch=$(uname -m)
 
-case "$arch" in
-    aarch64) url="https://github.com/nushell/nushell/releases/download/$version/nu-$version-aarch64-unknown-linux-musl.tar.gz"       ;;
-    *)       url="https://raw.githubusercontent.com/amtoine/nushell-builds/refs/heads/main/nu-$version-arm-unknown-linux-gnueabihf" ;;
+        case "$arch" in
+            aarch64) url="https://github.com/nushell/nushell/releases/download/$version/nu-$version-aarch64-unknown-linux-musl.tar.gz"       ;;
+            *)       url="https://raw.githubusercontent.com/amtoine/nushell-builds/refs/heads/main/nu-$version-arm-unknown-linux-gnueabihf" ;;
+        esac
+
+        mkdir -p "$bin_dir"
+
+        echo "Downloading $url..."
+        curl -fLo "$bin_dir/nu" "$url" || exit "$?"
+        chmod +x "$bin_dir/nu"
+    ;;
+    "cargo")
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+        export PATH="$HOME/.cargo/bin:$PATH"
+        cargo install "nu@$version"
+    ;;
+    *)
+        echo "unreachable"
+        exit 128
+    ;;
 esac
 
-mkdir -p "$bin_dir"
-
-echo "Downloading $url..."
-curl -fLo "$bin_dir/nu" "$url" || exit "$?"
-chmod +x "$bin_dir/nu"
+exit 0
